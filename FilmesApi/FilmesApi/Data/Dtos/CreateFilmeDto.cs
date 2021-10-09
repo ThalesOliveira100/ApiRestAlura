d4591bfb2f0f204d7d6da3bc9c64562f2b45fe86
Thales Oliveira <73021124+ThalesOliveira100@users.noreply.github.com>
2021-10-08 22:28:02 -0300

    Projeto do primeiro curso
    No curso foi ensinado as quatro operações básicas:
    
    Post, Get, Put e Delete.
    
    Foi criado um sistema que gerencia informações sobre filmes, (Nome, Diretor, Duração e Gêneros) utilizando o MySql como base de dados.
    No curso foi tratado também o pacote AutoMapper.

---
+++ FilmesApi/FilmesApi/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Data.Dtos
+{
+    public class CreateFilmeDto
+    {
+        [Required(ErrorMessage = "Campo titulo obrigatorio")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Campo diretor obrigatorio")]
+        public string Diretor { get; set; }
+
+        [StringLength(30, ErrorMessage = "O genero nao pode ultrapassar 30 caracteres")]
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "Duracao deve estar entre 1 a 600")]
+        public int Duracao { get; set; }
+    }
+}
---
+++ FilmesApi/FilmesApi/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo titulo obrigatorio")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Campo diretor obrigatorio")]
+        public string Diretor { get; set; }
+
+        [StringLength(30, ErrorMessage = "O genero nao pode ultrapassar 30 caracteres")]
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "Duracao deve estar entre 1 a 600")]
+        public int Duracao { get; set; }
+
+        public DateTime HoraDaConsulta { get; set; }
+    }
+}
